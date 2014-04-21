d80d9230a810c66b3135d5614356618222d28e96
hrach <hrach.cz@gmail.com>
2014-04-21 09:54:17 +0200

    [di] added Orm extension

---
+++ src/DI/OrmExtension.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * This file is part of the Nextras\ORM library.
+ *
+ * @license    MIT
+ * @link       https://github.com/nextras/orm
+ * @author     Jan Skrasek
+ */
+
+namespace Nextras\Orm\DI;
+
+use Nette\PhpGenerator;
+use Nette\DI\CompilerExtension;
+use Nette\Reflection\AnnotationsParser;
+use Nette\Reflection\ClassType;
+use Nextras\Orm\InvalidStateException;
+use Nextras\Orm\RuntimeException;
+
+
+class OrmExtension extends CompilerExtension
+{
+
+	public function loadConfiguration()
+	{
+		$config = $this->getConfig();
+		if (!isset($config['model'])) {
+			throw new InvalidStateException('Model is not defined.');
+		}
+	}
+
+
+	public function beforeCompile()
+	{
+		$config = $this->getConfig();
+		$repositories = $this->getRepositoryList($config['model']);
+
+		$this->setupRepositoriesAndMappers($repositories);
+		$this->setupModel($config['model'], $repositories);
+	}
+
+
+	private function setupRepositoriesAndMappers($repositories)
+	{
+		$builder = $this->getContainerBuilder();
+
+		foreach ($repositories as $repositoryData) {
+			$mapperName = $this->prefix('mappers.' . $repositoryData['name']);
+			if (!$builder->hasDefinition($mapperName)) {
+				$mapperClass = substr($repositoryData['class'], 0, -10) . 'Mapper';
+				if (!class_exists($mapperClass)) {
+					throw new InvalidStateException("Uknown mapper for '{$repositoryData['name']}' repository.");
+				}
+
+				$builder->addDefinition($mapperName)
+					->setClass($mapperClass);
+			}
+
+			$repositoryName = $this->prefix('repositories.' . $repositoryData['name']);
+			if (!$builder->hasDefinition($repositoryName)) {
+				$builder->addDefinition($repositoryName)
+					->setClass($repositoryData['class'])
+					->setArguments(['@' . $mapperName])
+					->addSetup('onModelAttach', ['@' . $this->prefix('model')]);
+			}
+		}
+	}
+
+
+	private function setupModel($modelClass, $repositories)
+	{
+		$builder = $this->getContainerBuilder();
+
+		$builder->addDefinition($this->prefix('model'))
+			->setClass($modelClass)
+			->setArguments(['repositories' => $repositories]);
+	}
+
+
+	private function getRepositoryList($modelClass)
+	{
+		$modelReflection = new ClassType($modelClass);
+
+		$builder = $this->getContainerBuilder();
+		$builder->addDependency($modelReflection->getFileName());
+
+		$repositories = [];
+		foreach ($modelReflection->getAnnotations() as $key => $annotations) {
+			if ($key !== 'property-read') {
+				continue;
+			}
+
+			foreach ($annotations as $annotation) {
+				list($class, $name) = preg_split('#\s+#', $annotation);
+				$class = AnnotationsParser::expandClassName($class, $modelReflection);
+				if (!class_exists($class)) {
+					throw new RuntimeException("Class repository '{$class}' does not exist.");
+				}
+
+				$repositories[] = [
+					'name' => ltrim($name, '$'),
+					'class' => $class,
+					'entities' => call_user_func([$class, 'getEntityClassNames']),
+				];
+			}
+		}
+
+		return $repositories;
+	}
+
+}
---
+++ src/exceptions.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of the Nextras\ORM library.
+ *
+ * @license    MIT
+ * @link       https://github.com/nextras/orm
+ * @author     Jan Skrasek
+ */
+
+namespace Nextras\Orm;
+
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+}
+
+
+class RuntimeException extends \RuntimeException
+{
+}
+
+
+class InvalidStateException extends RuntimeException
+{
+}
+
+
+class LogicException extends \LogicException
+{
+}
+
+
+class MemberAccessException extends LogicException
+{
+}
+
+
+class NotImplementedException extends LogicException
+{
+}
+
+
+class NotSupportedException extends LogicException
+{
+}
