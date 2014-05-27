81a828141c629be5e6db06ffc1e8364fc7bca999
hrach <hrach.cz@gmail.com>
2014-05-27 09:51:55 +0200

    [tests] added bootstraps

--- tests/bootstrap-model.php
+++ tests/bootstrap-model.php
@@ -1,5 +1,11 @@
 <?php
 
+namespace Nextras\Orm\Tests;
+
+use Nette\Database\Connection;
+use Nette\Database\Context;
+use Nette\Database\Conventions\DiscoveredConventions;
+use Nette\Database\Structure;
 use Model\AuthorsMapper;
 use Model\AuthorsRepository;
 use Model\BooksMapper;
@@ -9,6 +15,12 @@ use Model\TagsRepository;
 use Nextras\Orm\Model\StaticModel;
 
 require __DIR__ . '/bootstrap.php';
+
+
+$connection   = new Connection('mysql:host=localhost;dbname=nextras_orm_test', 'root', '', ['lazy' => TRUE]);
+$structure    = new Structure($connection, $cacheStorage);
+$conventions  = new DiscoveredConventions($structure);
+$context      = new Context($connection, $structure, $conventions, $cacheStorage);
 
 
 $model = new StaticModel([
---
+++ tests/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Nextras\Orm\Tests;
+
+use Nette\Caching\Storages\FileStorage;
+use Tester\Environment;
+use Tester\Helpers;
+
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+	echo "Install Nette Tester using `composer update`\n";
+	exit(1);
+}
+
+require_once __DIR__ . '/TestCase.php';
+
+Environment::setup();
+
+
+define('TEMP_DIR', __DIR__ . '/../tmp/' . getmypid());
+@mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
+Helpers::purge(TEMP_DIR);
+
+
+$cacheStorage = new FileStorage(TEMP_DIR);
