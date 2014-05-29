07f01fc18a95bfd80d68fb3d61d1db5a6c96d3f3
hrach <hrach.cz@gmail.com>
2014-05-29 21:21:48 +0200

    [tests] fixed loading testing model

--- tests/bootstrap-model.php
+++ tests/bootstrap-model.php
@@ -6,15 +6,25 @@ use Nette\Database\Connection;
 use Nette\Database\Context;
 use Nette\Database\Conventions\DiscoveredConventions;
 use Nette\Database\Structure;
-use Model\AuthorsMapper;
-use Model\AuthorsRepository;
-use Model\BooksMapper;
-use Model\BooksRepository;
-use Model\TagsMapper;
-use Model\TagsRepository;
+use Nextras\Orm\Tests\AuthorsMapper;
+use Nextras\Orm\Tests\AuthorsRepository;
+use Nextras\Orm\Tests\BooksMapper;
+use Nextras\Orm\Tests\BooksRepository;
+use Nextras\Orm\Tests\TagsMapper;
+use Nextras\Orm\Tests\TagsRepository;
 use Nextras\Orm\Model\StaticModel;
 
-require __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/bootstrap.php';
+require_once __DIR__ . '/model/author/Person.php';
+require_once __DIR__ . '/model/author/Author.php';
+require_once __DIR__ . '/model/author/AuthorsMapper.php';
+require_once __DIR__ . '/model/author/AuthorsRepository.php';
+require_once __DIR__ . '/model/book/Book.php';
+require_once __DIR__ . '/model/book/BooksMapper.php';
+require_once __DIR__ . '/model/book/BooksRepository.php';
+require_once __DIR__ . '/model/tag/Tag.php';
+require_once __DIR__ . '/model/tag/TagsMapper.php';
+require_once __DIR__ . '/model/tag/TagsRepository.php';
 
 
 $connection   = new Connection('mysql:host=localhost;dbname=nextras_orm_test', 'root', '', ['lazy' => TRUE]);
