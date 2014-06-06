5cc2d95afed092cfd4092067edf5942d562359cc
hrach <hrach.cz@gmail.com>
2014-06-06 20:05:01 +0200

    [tests] added read-only Author::$age property

--- tests/model/author/Author.php
+++ tests/model/author/Author.php
@@ -9,7 +9,18 @@ use Nextras\Orm\Relationships\OneHasMany;
  * @property string $web
  * @property OneHasMany|Book[] $books {1:m BooksRepository}
  * @property OneHasMany|Book[] $translatedBooks {1:m BooksRepository $translator}
+ * @property-read int $age
  */
 final class Author extends Person
 {
+
+	public function getAge()
+	{
+		if (!$this->born) {
+			return 0;
+		}
+
+		return date('Y') - $this->born->format('Y');
+	}
+
 }
