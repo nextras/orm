7b8aeb556c5a3efd1995642021e86f79d8b9ab02
hrach <hrach.cz@gmail.com>
2014-07-05 03:59:27 +0200

    [repository] fixed magic call

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -262,10 +262,10 @@ abstract class Repository extends Object implements IRepository
 	{
 		if (isset($this->proxyMethods[strtolower($method)])) {
 			if (substr($method, 0, 5) === 'getBy' || substr($method, 0, 6) === 'findBy') {
-				return call_user_func([$this->findAll(), $method], $args);
+				return call_user_func_array([$this->findAll(), $method], $args);
 			}
 
-			$result = call_user_func_array(array($this->mapper, $method), $args);
+			$result = call_user_func_array([$this->mapper, $method], $args);
 
 			if (!($result instanceof ICollection || $result instanceof IEntity)) {
 				$result = $this->mapper->toCollection($result);
