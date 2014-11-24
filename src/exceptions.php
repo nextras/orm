09f34d53d26373037d20db9fc5a999991f4e3699
hrach <hrach.cz@gmail.com>
2014-11-24 21:13:14 +0100

    exceptions: added NullValueException

--- src/exceptions.php
+++ src/exceptions.php
@@ -10,9 +10,24 @@
 
 namespace Nextras\Orm;
 
+use Nextras\Orm\Entity\IEntity;
+use Nextras\Orm\Entity\Reflection\PropertyMetadata;
+
 
 class InvalidArgumentException extends \InvalidArgumentException
 {
+}
+
+
+class NullValueException extends InvalidArgumentException
+{
+
+	public function __construct(IEntity $entity, PropertyMetadata $propertyMetadata)
+	{
+		$class = get_class($entity);
+		parent::__construct("Property {$class}::\${$propertyMetadata->name} is not nullable.");
+	}
+
 }
 
 
