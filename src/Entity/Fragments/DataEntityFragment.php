nextras/orm

commit 0e065df794966790f15a1f8833b1c09299e12be8

entity: introduced IProperty, added internal hasValue() impl [closes #54][refs #56]

src/Entity/Fragments/DataEntityFragment.php
@@ -11,6 +11,7 @@
 namespace Nextras\Orm\Entity\Fragments;
 
 use Nextras\Orm\Entity\IEntity;
+use Nextras\Orm\Entity\IProperty;
 use Nextras\Orm\Entity\IPropertyContainer;
 use Nextras\Orm\Entity\IPropertyHasRawValue;
 use Nextras\Orm\Entity\Reflection\EntityMetadata;
@@ -59,25 +60,13 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 	}
 
 
-	public function setValue($name, $value)
-	{
-		$metadata = $this->metadata->getProperty($name);
-		if ($metadata->isReadonly) {
-			throw new InvalidArgumentException("Property '$name' is read-only.");
-		}
-
-		$this->_setValue($metadata, $name, $value);
-		return $this;
-	}
-
-
 	public function isModified($name = NULL)
 	{
 		if ($name === NULL) {
 			return (bool) $this->modified;
 		}
 
-		$this->metadata->getProperty($name);
+		$this->metadata->getProperty($name); // checks property existance
 		return isset($this->modified[NULL]) || isset($this->modified[$name]);
 	}
 
@@ -89,18 +78,30 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 	}
 
 
-	public function setReadOnlyValue($name, $value)
+	public function setValue($name, $value)
 	{
 		$metadata = $this->metadata->getProperty($name);
-		$this->_setValue($metadata, $name, $value);
+		if ($metadata->isReadonly) {
+			throw new InvalidArgumentException("Property '$name' is read-only.");
+		}
+
+		$this->internalSetValue($metadata, $name, $value);
 		return $this;
 	}
 
 
-	public function & getValue($name, $allowNull = FALSE)
+	public function setReadOnlyValue($name, $value)
+	{
+		$metadata = $this->metadata->getProperty($name);
+		$this->internalSetValue($metadata, $name, $value);
+		return $this;
+	}
+
+
+	public function & getValue($name)
 	{
 		$property = $this->metadata->getProperty($name);
-		return $this->_getValue($property, $name, $allowNull);
+		return $this->internalGetValue($property, $name);
 	}
 
 
@@ -110,15 +111,16 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 			return FALSE;
 		}
 
-		$value = $this->_getValue($this->metadata->getProperty($name), $name, TRUE);
-		return isset($value);
+		return $this->internalHasValue($name);
 	}
 
 
 	public function & getRawValue($name)
 	{
-		$metadata = $this->metadata->getProperty($name);
-		$this->initDefaultValue($metadata);
+		$propertyMetadata = $this->metadata->getProperty($name);
+		if (!isset($this->validated[$name])) {
+			$this->initProperty($propertyMetadata, $name);
+		}
 
 		if ($this->data[$name] instanceof IPropertyHasRawValue) {
 			$value = $this->data[$name]->getRawValue();
@@ -131,14 +133,11 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 
 	public function getProperty($name)
 	{
-		$metadata = $this->metadata->getProperty($name);
-		$this->initDefaultValue($metadata);
-
-		if ($metadata->container && !is_object($this->data[$name])) {
-			$class = $metadata->container;
-			$this->data[$name] = new $class($this, $metadata, $this->data[$name]);
-			$this->validated[$name] = TRUE;
+		$propertyMetadata = $this->metadata->getProperty($name);
+		if (!isset($this->validated[$name])) {
+			$this->initProperty($propertyMetadata, $name);
 		}
+
 		return $this->data[$name];
 	}
 
@@ -146,103 +145,6 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 	public function toArray($mode = self::TO_ARRAY_RELATIONSHIP_AS_IS)
 	{
 		return ToArrayConverter::toArray($this, $mode);
-	}
-
-
-	protected function onLoad(IRepository $repository, EntityMetadata $metadata, array $data)
-	{
-		$this->metadata = $metadata;
-		foreach ($metadata->getStorageProperties() as $property) {
-			if (isset($data[$property])) {
-				$this->data[$property] = $data[$property];
-			}
-		}
-
-		parent::onLoad($repository, $metadata, $data);
-	}
-
-
-	protected function onAttach(IRepository $repository, EntityMetadata $metadata)
-	{
-		parent::onAttach($repository, $metadata);
-		$this->metadata = $metadata;
-	}
-
-
-	protected function onPersist($id)
-	{
-		$this->setValue('id', $id);
-		$this->modified = [];
-		parent::onPersist($id);
-	}
-
-
-	protected function _setValue(PropertyMetadata $metadata, $name, $value)
-	{
-		$this->initDefaultValue($metadata);
-
-		if ($metadata->container && !is_object($this->data[$name])) {
-			$class = $metadata->container;
-			$this->data[$name] = new $class($this, $metadata, $this->data[$name]);
-			$this->validated[$name] = TRUE;
-		}
-
-		if ($metadata->hasSetter && !isset($this->setterCall[$name])) {
-			$this->setterCall[$name] = TRUE;
-			call_user_func([$this, 'set' . $name], $value);
-			unset($this->setterCall[$name]);
-			return;
-		}
-
-		if ($this->data[$name] instanceof IPropertyContainer) {
-			$this->data[$name]->setInjectedValue($value);
-			$this->modified[$name] = $this->data[$name]->isModified();
-
-		} else {
-			if (!$metadata->isValid($value)) {
-				$class = get_class($this);
-				throw new InvalidArgumentException("Value for {$class}::\${$name} property is invalid.");
-			}
-			$this->data[$name] = $value;
-			$this->modified[$name] = TRUE;
-		}
-
-		$this->validated[$name] = TRUE;
-	}
-
-
-	protected function & _getValue(PropertyMetadata $metadata, $name, $allowNull = FALSE)
-	{
-		$this->initDefaultValue($metadata);
-
-		if (!$metadata->isReadonly && !isset($this->validated[$name]) && !$metadata->container) {
-			if (!($allowNull && empty($this->data[$name])) && in_array($name, $this->metadata->getStorageProperties(), TRUE)) {
-				$this->_setValue($metadata, $name, $this->data[$name]);
-				unset($this->modified[$name]);
-			}
-
-		} elseif ($metadata->container && !is_object($this->data[$name])) {
-			$class = $metadata->container;
-			$this->data[$name] = new $class($this, $metadata, $this->data[$name]);
-			$this->validated[$name] = TRUE;
-		}
-
-		if ($metadata->hasGetter && !isset($this->getterCall[$name])) {
-			$this->getterCall[$name] = TRUE;
-			$value = call_user_func([$this, 'get' . $name]);
-			unset($this->getterCall[$name]);
-			return $value;
-		}
-
-		if ($this->data[$name] instanceof IPropertyContainer) {
-			$value = $this->data[$name]->getInjectedValue($allowNull);
-			return $value;
-		} else {
-			if (!isset($this->data[$name]) && !$metadata->isNullable && !$allowNull) {
-				throw new InvalidStateException("Property '$name' is not set.");
-			}
-			return $this->data[$name];
-		}
 	}
 
 
@@ -291,7 +193,6 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 		$this->modified = $unserialized['modified'];
 		$this->validated = $unserialized['validated'];
 		$this->data = $unserialized['data'];
-
 	}
 
 
@@ -301,17 +202,147 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 	}
 
 
+	// === events ======================================================================================================
+
+
+	protected function onLoad(IRepository $repository, EntityMetadata $metadata, array $data)
+	{
+		$this->metadata = $metadata;
+		foreach ($metadata->getStorageProperties() as $property) {
+			if (isset($data[$property])) {
+				$this->data[$property] = $data[$property];
+			}
+		}
+
+		parent::onLoad($repository, $metadata, $data);
+	}
+
+
+	protected function onAttach(IRepository $repository, EntityMetadata $metadata)
+	{
+		parent::onAttach($repository, $metadata);
+		$this->metadata = $metadata;
+	}
+
+
+	protected function onPersist($id)
+	{
+		$this->setValue('id', $id);
+		$this->modified = [];
+		parent::onPersist($id);
+	}
+
+
+	// === internal implementation =====================================================================================
+
+
 	protected function createMetadata()
 	{
 		return MetadataStorage::get(get_class($this));
 	}
 
 
-	protected function initDefaultValue(PropertyMetadata $propertyMetadata)
+	private function internalSetValue(PropertyMetadata $metadata, $name, $value)
 	{
-		if (!array_key_exists($propertyMetadata->name, $this->data)) {
-			$this->data[$propertyMetadata->name] = $propertyMetadata->defaultValue;
+		if (!isset($this->validated[$name])) {
+			$this->initProperty($metadata, $name);
 		}
+
+		if ($metadata->hasSetter && !isset($this->setterCall[$name])) {
+			$this->setterCall[$name] = TRUE;
+			call_user_func([$this, 'set' . $name], $value);
+			unset($this->setterCall[$name]);
+			return;
+		}
+
+		if ($this->data[$name] instanceof IPropertyContainer) {
+			$this->data[$name]->setInjectedValue($value);
+
+		} else {
+			if (!$metadata->isValid($value)) {
+				$class = get_class($this);
+				throw new InvalidArgumentException("Value for {$class}::\${$name} property is invalid.");
+			}
+			$this->data[$name] = $value;
+			$this->modified[$name] = TRUE;
+		}
+	}
+
+
+	private function & internalGetValue(PropertyMetadata $propertyMetadata, $name)
+	{
+		if (!isset($this->validated[$name])) {
+			$this->initProperty($propertyMetadata, $name);
+		}
+
+		if ($propertyMetadata->hasGetter && !isset($this->getterCall[$name])) {
+			$this->getterCall[$name] = TRUE;
+			$value = call_user_func([$this, 'get' . $name]);
+			unset($this->getterCall[$name]);
+			return $value;
+		}
+
+		if ($this->data[$name] instanceof IPropertyContainer) {
+			return $this->data[$name]->getInjectedValue();
+
+		} else {
+			if (!isset($this->data[$name]) && !$propertyMetadata->isNullable) {
+				$class = get_class($this);
+				throw new InvalidStateException("Property {$class}::\${$name} is not set.");
+			}
+			return $this->data[$name];
+		}
+	}
+
+
+	private function internalHasValue($name)
+	{
+		if (!isset($this->validated[$name])) {
+			$this->initProperty($this->metadata->getProperty($name), $name);
+		}
+
+		if ($this->data[$name] instanceof IPropertyContainer) {
+			return $this->data[$name]->hasInjectedValue();
+
+		} else {
+			return isset($this->data[$name]);
+		}
+	}
+
+
+	private function initProperty(PropertyMetadata $propertyMetadata, $name)
+	{
+		$this->validated[$name] = TRUE;
+
+		if (!array_key_exists($name, $this->data)) {
+			$this->data[$name] = $propertyMetadata->defaultValue;
+		}
+
+		if ($propertyMetadata->container) {
+			$this->initPropertyObject($propertyMetadata, $name);
+
+		} elseif ($this->data[$name] !== NULL) {
+			$this->internalSetValue($propertyMetadata, $name, $this->data[$name]);
+			unset($this->modified[$name]);
+		}
+	}
+
+
+	private function initPropertyObject(PropertyMetadata $propertyMetadata, $name)
+	{
+		$class = $propertyMetadata->container;
+
+		/** @var IProperty $property */
+		$property = new $class($this, $propertyMetadata);
+		$property->onModify(function() use ($name) {
+			$this->modified[$name] = TRUE;
+		});
+
+		if ($this->isPersisted()) {
+			$property->setLoadedValue($this->data[$name]);
+		}
+
+		$this->data[$name] = $property;
 	}
 
 }

src/Entity/IProperty.php
@@ -1,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Nextras\ORM library.
+ *
+ * @license    MIT
+ * @link       https://github.com/nextras/orm
+ * @author     Jan Skrasek
+ */
+
+namespace Nextras\Orm\Entity;
+
+use Nextras\Orm\Entity\Reflection\PropertyMetadata;
+
+
+interface IProperty
+{
+
+	public function __construct(IEntity $entity, PropertyMetadata $propertyMetadata);
+
+
+	/**
+	 * Sets value loaded from database.
+	 * @param  mixed $value
+	 */
+	public function setLoadedValue($value);
+
+
+	/**
+	 * Adds onModify callback notification.
+	 * @param  mixed $callback
+	 */
+	public function onModify($callback);
+
+}

src/Relationships/HasMany.php
@@ -11,6 +11,7 @@
 namespace Nextras\Orm\Relationships;
 
 use Nette\Object;
+use Nette\Utils\Callback;
 use Nextras\Orm\Entity\Collection\ArrayCollection;
 use Nextras\Orm\Entity\Collection\ICollection;
 use Nextras\Orm\Entity\IEntity;
@@ -48,18 +49,32 @@ abstract class HasMany extends Object implements IRelationshipCollection
 	/** @var bool */
 	protected $isModified = FALSE;
 
+	/** @var array */
+	protected $onModify = [];
 
-	public function __construct(IEntity $parent, PropertyMetadata $metadata, $value)
+
+	public function __construct(IEntity $parent, PropertyMetadata $metadata)
 	{
 		$this->parent = $parent;
 		$this->metadata = $metadata;
-		$this->injectedValue = $value;
 	}
 
 
 	public function setParent(IEntity $parent)
 	{
 		$this->parent = $parent;
+	}
+
+
+	public function setLoadedValue($value)
+	{
+		$this->injectedValue = $value;
+	}
+
+
+	public function onModify($callback)
+	{
+		$this->onModify[] = Callback::check($callback);
 	}
 
 
@@ -79,7 +94,7 @@ abstract class HasMany extends Object implements IRelationshipCollection
 		}
 
 		$this->updateRelationshipAdd($entity);
-		$this->isModified = TRUE;
+		$this->modify();
 		$this->collection = NULL;
 		return $entity;
 	}
@@ -102,7 +117,7 @@ abstract class HasMany extends Object implements IRelationshipCollection
 		}
 
 		$this->updateRelationshipRemove($entity);
-		$this->isModified = TRUE;
+		$this->modify();
 		$this->collection = NULL;
 		return $entity;
 	}
@@ -310,6 +325,15 @@ abstract class HasMany extends Object implements IRelationshipCollection
 			return $collection->orderBy($this->metadata->args->relationship['order'][0], $this->metadata->args->relationship['order'][1]);
 		} else {
 			return $collection;
+		}
+	}
+
+
+	protected function modify()
+	{
+		$this->isModified = TRUE;
+		foreach ($this->onModify as $callback) {
+			Callback::invoke($callback);
 		}
 	}
 

src/Relationships/HasOne.php
@@ -11,6 +11,7 @@
 namespace Nextras\Orm\Relationships;
 
 use Nette\Object;
+use Nette\Utils\Callback;
 use Nextras\Orm\Entity\Collection\ICollection;
 use Nextras\Orm\Entity\IEntity;
 use Nextras\Orm\Entity\Reflection\PropertyMetadata;
@@ -44,12 +45,14 @@ abstract class HasOne extends Object implements IRelationshipContainer, Database
 	/** @var bool */
 	protected $isModified;
 
+	/** @var array */
+	protected $onModify = [];
 
-	public function __construct(IEntity $parent, PropertyMetadata $propertyMeta, $value)
+
+	public function __construct(IEntity $parent, PropertyMetadata $propertyMeta)
 	{
 		$this->parent = $parent;
 		$this->propertyMeta = $propertyMeta;
-		$this->primaryValue = $value;
 	}
 
 
@@ -59,21 +62,40 @@ abstract class HasOne extends Object implements IRelationshipContainer, Database
 	}
 
 
+	public function setLoadedValue($value)
+	{
+		$this->primaryValue = $value;
+	}
+
+
+	public function onModify($callback)
+	{
+		$this->onModify[] = Callback::check($callback);
+	}
+
+
 	public function setInjectedValue($value)
 	{
 		$this->set($value);
 	}
 
 
-	public function isLoaded()
+	public function & getInjectedValue()
 	{
-		return $this->value !== FALSE;
+		$value = $this->getEntity(FALSE);
+		return $value;
 	}
 
 
-	public function getInjectedValue($allowNull = FALSE)
+	public function hasInjectedValue()
 	{
-		return $this->getEntity($allowNull);
+		return $this->getEntity(TRUE) !== NULL;
+	}
+
+
+	public function isLoaded()
+	{
+		return $this->value !== FALSE;
 	}
 
 
@@ -104,7 +126,7 @@ abstract class HasOne extends Object implements IRelationshipContainer, Database
 		$value = $this->createEntity($value, $allowNull);
 
 		if ($this->isChanged($value)) {
-			$this->isModified = TRUE;
+			$this->modify();
 			$oldValue = $this->value;
 			if ($oldValue === FALSE) {
 				$primaryValue = $this->getPrimaryValue();
@@ -232,6 +254,15 @@ abstract class HasOne extends Object implements IRelationshipContainer, Database
 
 		} else {
 			return $newValue !== $this->value;
+		}
+	}
+
+
+	protected function modify()
+	{
+		$this->isModified = TRUE;
+		foreach ($this->onModify as $callback) {
+			Callback::invoke($callback);
 		}
 	}
 

src/Relationships/IRelationshipCollection.php
@@ -14,10 +14,11 @@ use Countable;
 use IteratorAggregate;
 use Nextras\Orm\Entity\IEntity;
 use Nextras\Orm\Entity\Collection\ICollection;
+use Nextras\Orm\Entity\IProperty;
 use Nextras\Orm\Entity\IPropertyHasRawValue;
 
 
-interface IRelationshipCollection extends IPropertyHasRawValue, IteratorAggregate, Countable
+interface IRelationshipCollection extends IProperty, IPropertyHasRawValue, IteratorAggregate, Countable
 {
 
 	/**

src/Entity/Fragments/RepositoryEntityFragment.php
@@ -82,6 +82,20 @@ abstract class RepositoryEntityFragment extends EventEntityFragment implements I
 	}
 
 
+	public function serialize()
+	{
+		return [
+			'persistedId' => $this->persistedId,
+		];
+	}
+
+
+	public function unserialize($unserialized)
+	{
+		$this->persistedId = $unserialized['persistedId'];
+	}
+
+
 	protected function onAttach(IRepository $repository, EntityMetadata $metadata)
 	{
 		parent::onAttach($repository, $metadata);
@@ -93,6 +107,7 @@ abstract class RepositoryEntityFragment extends EventEntityFragment implements I
 	{
 		parent::onLoad($repository, $metadata, $data);
 		$this->repository = $repository;
+		$this->persistedId = TRUE; // id getter see entity already as persisted
 		$this->persistedId = $this->getValue('id');
 	}
 
@@ -119,20 +134,6 @@ abstract class RepositoryEntityFragment extends EventEntityFragment implements I
 		}
 
 		$this->repository = $repository;
-	}
-
-
-	public function serialize()
-	{
-		return [
-			'persistedId' => $this->persistedId,
-		];
-	}
-
-
-	public function unserialize($unserialized)
-	{
-		$this->persistedId = $unserialized['persistedId'];
 	}
 
 }

src/Entity/IEntity.php
@@ -89,11 +89,9 @@ interface IEntity extends Serializable
 	/**
 	 * Returns value.
 	 * @param  string   $name
-	 * @param  bool     $allowNull
-	 * @param  ICollection
 	 * @return mixed
 	 */
-	public function getValue($name, $allowNull = FALSE);
+	public function getValue($name);
 
 
 	/**

src/Entity/IPropertyContainer.php
@@ -11,7 +11,7 @@
 namespace Nextras\Orm\Entity;
 
 
-interface IPropertyContainer
+interface IPropertyContainer extends IProperty
 {
 
 	/**
@@ -25,16 +25,15 @@ interface IPropertyContainer
 	/**
 	 * Returns injected value.
 	 * @internal
-	 * @param  bool
 	 * @return mixed
 	 */
-	public function getInjectedValue($allowNull = FALSE);
+	public function & getInjectedValue();
 
 
 	/**
-	 * Returns true if modified.
+	 * Returns wheter property container has a value.
 	 * @return bool
 	 */
-	public function isModified();
+	public function hasInjectedValue();
 
 }

commit 3079c18d39254748ea272fe7205c23bc981ec606

entity: removed IPropertyHasRawValue, fixed regression
Fixed performance regression from 0e065df794966790f15a1f8833b1c09299e12be8

src/Entity/Fragments/DataEntityFragment.php
@@ -13,7 +13,6 @@ namespace Nextras\Orm\Entity\Fragments;
 use Nextras\Orm\Entity\IEntity;
 use Nextras\Orm\Entity\IProperty;
 use Nextras\Orm\Entity\IPropertyContainer;
-use Nextras\Orm\Entity\IPropertyHasRawValue;
 use Nextras\Orm\Entity\Reflection\EntityMetadata;
 use Nextras\Orm\Entity\Reflection\PropertyMetadata;
 use Nextras\Orm\Entity\ToArrayConverter;
@@ -63,7 +62,7 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 			return (bool) $this->modified;
 		}
 
-		$this->metadata->getProperty($name); // checks property existance
+		$this->metadata->getProperty($name); // checks property existence
 		return isset($this->modified[NULL]) || isset($this->modified[$name]);
 	}
 
@@ -126,12 +125,11 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 
 	public function & getRawValue($name)
 	{
-		$propertyMetadata = $this->metadata->getProperty($name);
-		if (!isset($this->validated[$name])) {
-			$this->initProperty($propertyMetadata, $name);
-		}
+		$this->metadata->getProperty($name); // checks property existence
 
-		if ($this->data[$name] instanceof IPropertyHasRawValue) {
+		if (!isset($this->data[$name])) {
+			return NULL;
+		} elseif ($this->data[$name] instanceof IProperty) {
 			$value = $this->data[$name]->getRawValue();
 			return $value;
 		} else {
@@ -344,7 +342,7 @@ abstract class DataEntityFragment extends RepositoryEntityFragment implements IE
 			});
 
 			if ($this->isPersisted()) {
-				$property->setLoadedValue($this->data[$name]);
+				$property->setRawValue($this->data[$name]);
 			}
 
 			$this->data[$name] = $property;

src/Entity/IProperty.php
@@ -20,16 +20,25 @@ interface IProperty
 
 
 	/**
-	 * Sets value loaded from database.
-	 * @param  mixed $value
-	 */
-	public function setLoadedValue($value);
-
-
-	/**
 	 * Adds onModify callback notification.
 	 * @param  mixed $callback
 	 */
 	public function onModify($callback);
+
+
+	/**
+	 * Sets raw value (value loaded from database).
+	 * Raw value is the same value as when the container has not yet been created.
+	 * @param  mixed $value
+	 */
+	public function setRawValue($value);
+
+
+	/**
+	 * Returns raw value.
+	 * Raw value is the same value as when the container has not yet been created.
+	 * @return mixed
+	 */
+	public function getRawValue();
 
 }

src/Entity/IPropertyHasRawValue.php
@@ -1,23 +1,0 @@
-<?php
-
-/**
- * This file is part of the Nextras\ORM library.
- *
- * @license    MIT
- * @link       https://github.com/nextras/orm
- * @author     Jan Skrasek
- */
-
-namespace Nextras\Orm\Entity;
-
-
-interface IPropertyHasRawValue
-{
-
-	/**
-	 * Returns raw value.
-	 * @return mixed
-	 */
-	public function getRawValue();
-
-}

src/Relationships/HasMany.php
@@ -66,7 +66,7 @@ abstract class HasMany extends Object implements IRelationshipCollection
 	}
 
 
-	public function setLoadedValue($value)
+	public function setRawValue($value)
 	{
 		$this->injectedValue = $value;
 	}

src/Relationships/HasOne.php
@@ -62,9 +62,15 @@ abstract class HasOne extends Object implements IRelationshipContainer, Database
 	}
 
 
-	public function setLoadedValue($value)
+	public function setRawValue($value)
 	{
 		$this->primaryValue = $value;
+	}
+
+
+	public function getRawValue()
+	{
+		return $this->getPrimaryValue();
 	}
 
 
@@ -96,12 +102,6 @@ abstract class HasOne extends Object implements IRelationshipContainer, Database
 	public function isLoaded()
 	{
 		return $this->value !== FALSE;
-	}
-
-
-	public function getRawValue()
-	{
-		return $this->getPrimaryValue();
 	}
 
 

src/Relationships/IRelationshipCollection.php
@@ -15,10 +15,9 @@ use IteratorAggregate;
 use Nextras\Orm\Entity\IEntity;
 use Nextras\Orm\Entity\Collection\ICollection;
 use Nextras\Orm\Entity\IProperty;
-use Nextras\Orm\Entity\IPropertyHasRawValue;
 
 
-interface IRelationshipCollection extends IProperty, IPropertyHasRawValue, IteratorAggregate, Countable
+interface IRelationshipCollection extends IProperty, IteratorAggregate, Countable
 {
 
 	/**

src/Relationships/IRelationshipContainer.php
@@ -12,10 +12,9 @@ namespace Nextras\Orm\Relationships;
 
 use Nextras\Orm\Entity\IEntity;
 use Nextras\Orm\Entity\IPropertyContainer;
-use Nextras\Orm\Entity\IPropertyHasRawValue;
 
 
-interface IRelationshipContainer extends IPropertyContainer, IPropertyHasRawValue
+interface IRelationshipContainer extends IPropertyContainer
 {
 
 	/**

src/Relationships/OneHasMany.php
@@ -10,8 +10,8 @@
 
 namespace Nextras\Orm\Relationships;
 
-use Nette\NotImplementedException;
 use Nextras\Orm\Entity\IEntity;
+use Nextras\Orm\NotImplementedException;
 
 
 class OneHasMany extends HasMany