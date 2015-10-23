83b430da074551368289c11b436abc952a8b7294
Mikulas <rullaf@gmail.com>
2015-10-23 16:27:34 +0200

    breaking change

--- tests/inc/model/publisher/Publisher.php
+++ tests/inc/model/publisher/Publisher.php
@@ -3,7 +3,9 @@
 namespace NextrasTests\Orm;
 
 use Nextras\Orm\Entity\Entity;
+use Nextras\Orm\Entity\Reflection\EntityMetadata;
 use Nextras\Orm\Relationships\OneHasMany as OHM;
+use Nextras\Orm\Repository\IRepository;
 
 
 /**
@@ -13,4 +15,12 @@ use Nextras\Orm\Relationships\OneHasMany as OHM;
  */
 final class Publisher extends Entity
 {
+
+	protected function onAttach(IRepository $repository, EntityMetadata $metadata)
+	{
+		parent::onAttach($repository, $metadata);
+
+		$this->location; // trigger ORM magic
+	}
+
 }
