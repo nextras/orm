0e14e12bfb639917f9e496027dbca3a1eed22214
Jan Skrasek <hrach.cz@gmail.com>
2016-01-23 13:33:22 +0100

    tests: added new entities with cycle

---
+++ tests/inc/model/photoAlbum/PhotoAlbum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace NextrasTests\Orm;
+
+use Nextras\Orm\Entity\Entity;
+use Nextras\Orm\Relationships\OneHasMany as OHM;
+
+
+/**
+ * @property int            $id {primary}
+ * @property string         $title
+ * @property Photo[]|OHM    $photos {1:m Photo::$album}
+ * @property Photo|null     $preview {1:1 Photo::$previewFor, isMain=true}
+ */
+final class PhotoAlbum extends Entity
+{
+}
---
+++ tests/inc/model/photoAlbum/PhotoAlbumsRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace NextrasTests\Orm;
+
+use Nextras\Orm\Repository\Repository;
+
+
+/**
+ * @method PhotoAlbum|NULL getById($id)
+ */
+final class PhotoAlbumsRepository extends Repository
+{
+	static function getEntityClassNames()
+	{
+		return [PhotoAlbum::class];
+	}
+}
