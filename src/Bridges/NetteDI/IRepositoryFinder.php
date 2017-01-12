0af0c911a5dddf4f58fc4184f921beff6c393403
Jan Skrasek <hrach.cz@gmail.com>
2017-01-12 23:00:20 +0100

    model/di: repository finder refactored to own class & interface (BC break!)

---
+++ src/Bridges/NetteDI/IRepositoryFinder.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Nextras\Orm library.
+ * @license    MIT
+ * @link       https://github.com/nextras/orm
+ */
+
+namespace Nextras\Orm\Bridges\NetteDI;
+
+use Nette\DI\ContainerBuilder;
+
+
+interface IRepositoryFinder
+{
+	/**
+	 * @return array
+	 */
+	public function initRepositories($modelClass, ContainerBuilder $containerBuilder, callable $prefixCb);
+}
---
+++ src/Bridges/NetteDI/PhpDocRepositoryFinder.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * This file is part of the Nextras\Orm library.
+ * @license    MIT
+ * @link       https://github.com/nextras/orm
+ */
+
+namespace Nextras\Orm\Bridges\NetteDI;
+
+use Nette\DI\ContainerBuilder;
+use Nette\Utils\Reflection;
+use Nextras\Orm\InvalidStateException;
+use Nextras\Orm\Model\Model;
+use Nextras\Orm\RuntimeException;
+
+
+class PhpDocRepositoryFinder implements IRepositoryFinder
+{
+	/** @var ContainerBuilder */
+	protected $builder;
+
+	/** @var callable */
+	protected $prefixCb;
+
+
+	public function initRepositories($modelClass, ContainerBuilder $containerBuilder, callable $prefixCb)
+	{
+		$this->builder = $containerBuilder;
+		$this->prefixCb = $prefixCb;
+
+		$repositories = $this->findRepositories($modelClass);
+		$repositoriesMap = [];
+		foreach ($repositories as $repositoryName => $repositoryClass) {
+			$this->setupMapperService($repositoryName, $repositoryClass);
+			$this->setupRepositoryService($repositoryName, $repositoryClass);
+			$repositoriesMap[$repositoryClass] = $this->prefix('repositories.' . $repositoryName);
+		}
+
+		$this->setupRepositoryLoader($repositoriesMap);
+		return $repositories;
+	}
+
+
+	protected function prefix($name)
+	{
+		return call_user_func($this->prefixCb, $name);
+	}
+
+
+	protected function findRepositories($modelClass)
+	{
+		if ($modelClass === Model::class) {
+			throw new InvalidStateException('Your model has to inherit from ' . Model::class . '. Use compiler extension configuration - model key.');
+		}
+
+		$modelReflection = new \ReflectionClass($modelClass);
+		$this->builder->addDependency($modelReflection->getFileName());
+
+		$repositories = [];
+		preg_match_all(
+			'~^  [ \t*]*  @property(?:|-read)  [ \t]+  ([^\s$]+)  [ \t]+  \$  (\w+)  ()~mx',
+			(string) $modelReflection->getDocComment(), $matches, PREG_SET_ORDER
+		);
+
+		foreach ($matches as list(, $type, $name)) {
+			$type = Reflection::expandClassName($type, $modelReflection);
+			if (!class_exists($type)) {
+				throw new RuntimeException("Repository '{$type}' does not exist.");
+			}
+
+			$repositories[$name] = $type;
+		}
+
+		return $repositories;
+	}
+
+
+	protected function setupMapperService($repositoryName, $repositoryClass)
+	{
+		$mapperName = $this->prefix('mappers.' . $repositoryName);
+		if ($this->builder->hasDefinition($mapperName)) {
+			return;
+		}
+
+		$mapperClass = str_replace('Repository', 'Mapper', $repositoryClass);
+		if (!class_exists($mapperClass)) {
+			throw new InvalidStateException("Unknown mapper for '{$repositoryName}' repository.");
+		}
+
+		$this->builder->addDefinition($mapperName)
+			->setClass($mapperClass)
+			->setArguments([
+				'cache' => $this->prefix('@cache'),
+			]);
+	}
+
+
+	protected function setupRepositoryService($repositoryName, $repositoryClass)
+	{
+		$serviceName = $this->prefix('repositories.' . $repositoryName);
+		if ($this->builder->hasDefinition($serviceName)) {
+			return;
+		}
+
+		$this->builder->addDefinition($serviceName)
+			->setClass($repositoryClass)
+			->setArguments([
+				$this->prefix('@mappers.' . $repositoryName),
+				$this->prefix('@dependencyProvider'),
+			])
+			->addSetup('setModel', [$this->prefix('@model')]);
+	}
+
+
+	protected function setupRepositoryLoader(array $repositoriesMap)
+	{
+		$this->builder->addDefinition($this->prefix('repositoryLoader'))
+			->setClass(RepositoryLoader::class)
+			->setArguments([
+				'repositoryNamesMap' => $repositoriesMap,
+			]);
+	}
+}
