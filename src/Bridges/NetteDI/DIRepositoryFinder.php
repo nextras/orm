1c50fa2f57b0feca706bd022bf1acaca8e60ea6d
Jan Skrasek <hrach.cz@gmail.com>
2017-01-12 23:00:20 +0100

    model/di: added Nette DI Container repository loader [closes #182]

---
+++ src/Bridges/NetteDI/DIRepositoryFinder.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the Nextras\Orm library.
+ * @license    MIT
+ * @link       https://github.com/nextras/orm
+ */
+
+namespace Nextras\Orm\Bridges\NetteDI;
+
+use Nette\DI\ContainerBuilder;
+use Nextras\Orm\Repository\IRepository;
+
+
+class DIRepositoryFinder implements IRepositoryFinder
+{
+	public function initRepositories($modelClass, ContainerBuilder $containerBuilder, callable $prefixCb)
+	{
+		$types = $containerBuilder->findByType(IRepository::class);
+		$repositories = [];
+		$repositoriesMap = [];
+		foreach ($types as $serviceName => $serviceDefinition) {
+			$serviceDefinition->addSetup('setModel', [$prefixCb('@model')]);
+			$class = $serviceDefinition->getClass();
+			$name = lcfirst(str_ireplace('repository', '', substr($class, strrpos($class, '\\') + 1 ?: 0)));
+			$repositories[$name] = $class;
+			$repositoriesMap[$class] = $serviceName;
+		}
+
+		$this->setupRepositoryLoader($repositoriesMap, $containerBuilder, $prefixCb);
+		return $repositories;
+	}
+
+
+	protected function setupRepositoryLoader(array $repositoriesMap, ContainerBuilder $builder, callable $prefixCb)
+	{
+		$builder->addDefinition($prefixCb('repositoryLoader'))
+			->setClass(RepositoryLoader::class)
+			->setArguments([
+				'repositoryNamesMap' => $repositoriesMap,
+			]);
+	}
+}
