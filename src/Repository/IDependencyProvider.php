0842eea3cf02171c657b0613492f711718694890
Jan Skrasek <hrach.cz@gmail.com>
2017-01-14 13:53:34 +0100

    php7: enabled strict types

--- src/Repository/IDependencyProvider.php
+++ src/Repository/IDependencyProvider.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
  * This file is part of the Nextras\Orm library.
@@ -7,7 +7,6 @@
  */
 
 namespace Nextras\Orm\Repository;
-
 
 use Nextras\Orm\Entity\IEntity;
 
