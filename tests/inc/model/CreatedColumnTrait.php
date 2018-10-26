8fbdb0a37b49eec08c2c90c948fe9a39553d0f3d
roman.kovaricek <rkovaricek@centrum.cz>
2018-10-26 10:13:51 +0200

    loading annotation from traits too
    Test for loading trait annotation - created column

---
+++ tests/inc/model/CreatedColumnTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace NextrasTests\Orm;
+
+use DateTimeImmutable;
+
+/**
+ * @property DateTimeImmutable    $createdAt             {default now}
+ * @property-read string          $createdAtFormatted    {virtual}
+ */
+trait CreatedColumnTrait {
+
+    /**
+     * Getter for column createdAtFormatted
+     * @return string
+     */
+    protected function getterCreatedAtFormatted() : string {
+        return $this->createdAt->format('d.m.Y H:i:s');
+    }
+}
