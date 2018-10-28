9bba418db80b4140b5d61d7ad82dd79af9a5dab8
Jan Skrasek <hrach.cz@gmail.com>
2018-10-28 11:16:30 +0100

    php7.1: added constant visibility modifier

--- src/Collection/Helpers/ConditionParserHelper.php
+++ src/Collection/Helpers/ConditionParserHelper.php
@@ -14,12 +14,12 @@ use Nextras\Orm\InvalidArgumentException;
 class ConditionParserHelper
 {
 	/** @const operators */
-	const OPERATOR_EQUAL = '=';
-	const OPERATOR_NOT_EQUAL = '!=';
-	const OPERATOR_GREATER = '>';
-	const OPERATOR_EQUAL_OR_GREATER = '>=';
-	const OPERATOR_SMALLER = '<';
-	const OPERATOR_EQUAL_OR_SMALLER = '<=';
+	public const OPERATOR_EQUAL = '=';
+	public const OPERATOR_NOT_EQUAL = '!=';
+	public const OPERATOR_GREATER = '>';
+	public const OPERATOR_EQUAL_OR_GREATER = '>=';
+	public const OPERATOR_SMALLER = '<';
+	public const OPERATOR_EQUAL_OR_SMALLER = '<=';
 
 
 	public static function parsePropertyOperator(string $condition): array
