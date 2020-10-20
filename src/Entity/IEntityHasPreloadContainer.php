560e6058af362a834759078e60a51cccfdc81a16
Jan Skrasek <hrach.cz@gmail.com>
2020-10-20 22:39:24 +0200

    typo

--- src/Entity/IEntityHasPreloadContainer.php
+++ src/Entity/IEntityHasPreloadContainer.php
@@ -9,7 +9,7 @@ use Nextras\Orm\Collection\IEntityPreloadContainer;
 interface IEntityHasPreloadContainer
 {
 	/**
-	 * Sets the collection of entites for the loading relations at once.
+	 * Sets the collection of entities for the loading relations at once.
 	 */
 	public function setPreloadContainer(?IEntityPreloadContainer $overIterator): void;
 
