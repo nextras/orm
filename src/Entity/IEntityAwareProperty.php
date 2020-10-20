53c64d117529f0d11d75c18345c68936ea90b486
Jan Skrasek <hrach.cz@gmail.com>
2020-10-20 22:41:24 +0200

    entity: mark IENtityAwareProperty & IEmbeddable interfaces as experimental

--- src/Entity/IEntityAwareProperty.php
+++ src/Entity/IEntityAwareProperty.php
@@ -3,6 +3,9 @@
 namespace Nextras\Orm\Entity;
 
 
+/**
+ * @experimental This interface API is experimental and is subjected to change. It is ok to use its implementation.
+ */
 interface IEntityAwareProperty extends IProperty
 {
 	public function setPropertyEntity(IEntity $entity): void;
