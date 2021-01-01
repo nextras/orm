1ac939cb25a47496d61ca47f0a256411ef95df02
Jan Skrasek <hrach.cz@gmail.com>
2021-01-01 13:49:31 +0100

    fixes issues found by latest PHPStan

--- src/Mapper/Dbal/RelationshipMapperManyHasMany.php
+++ src/Mapper/Dbal/RelationshipMapperManyHasMany.php
@@ -164,8 +164,8 @@ class RelationshipMapperManyHasMany implements IRelationshipMapperManyHasMany
 		}
 
 		$values = [];
-		foreach ($result as $row) {
-			$values[$row->{$this->primaryKeyTo}] = null;
+		foreach ($result as $groupingRow) {
+			$values[$groupingRow->{$this->primaryKeyTo}] = null;
 		}
 
 		if (count($values) === 0) {
