dc728d900a5f2f1adb47078fdb015c292027e2de
Jan Skrasek <hrach.cz@gmail.com>
2024-04-11 15:28:49 +0200

    fix for Sequence names resolution and processing in the latest Dbal 5.0@dev

--- tests/sqls/NextrasTests/Orm/Integration/Collection/CollectionWhereTest_testFilterByPropertyWrapper.sql
+++ tests/sqls/NextrasTests/Orm/Integration/Collection/CollectionWhereTest_testFilterByPropertyWrapper.sql
@@ -1,11 +1,11 @@
 SELECT "books".* FROM "books" AS "books" WHERE (("books"."id" = 1));
 START TRANSACTION;
 INSERT INTO "eans" ("code", "type") VALUES ('123', 2);
-SELECT CURRVAL('"eans_id_seq"');
+SELECT CURRVAL('public.eans_id_seq');
 UPDATE "books" SET "ean_id" = 1 WHERE "id" = 1;
 SELECT "books".* FROM "books" AS "books" WHERE (("books"."id" = 2));
 INSERT INTO "eans" ("code", "type") VALUES ('456', 1);
-SELECT CURRVAL('"eans_id_seq"');
+SELECT CURRVAL('public.eans_id_seq');
 UPDATE "books" SET "ean_id" = 2 WHERE "id" = 2;
 COMMIT;
 SELECT "eans".* FROM "eans" AS "eans";
--- tests/sqls/NextrasTests/Orm/Integration/Mapper/ConventionsTest_testTimezoneDetection.sql
+++ tests/sqls/NextrasTests/Orm/Integration/Mapper/ConventionsTest_testTimezoneDetection.sql
@@ -1,9 +1,9 @@
 START TRANSACTION;
 INSERT INTO "public"."authors" ("name", "born", "web", "favorite_author_id") VALUES ('A', '2021-03-21 08:23:00.000000'::timestamp, 'http://www.example.com', NULL);
-SELECT CURRVAL('"authors_id_seq"');
+SELECT CURRVAL('public.authors_id_seq');
 INSERT INTO "publishers" ("name") VALUES ('P');
-SELECT CURRVAL('"publishers_publisher_id_seq"');
+SELECT CURRVAL('public.publishers_publisher_id_seq');
 INSERT INTO "books" ("title", "author_id", "translator_id", "next_part", "ean_id", "publisher_id", "genre", "published_at", "printed_at", "price", "price_currency", "orig_price_cents", "orig_price_currency") VALUES ('B', 3, NULL, NULL, NULL, 4, 'fantasy', '2015-09-09 10:10:10.000000'::timestamp, NULL, NULL, NULL, NULL, NULL);
-SELECT CURRVAL('"books_id_seq"');
+SELECT CURRVAL('public.books_id_seq');
 COMMIT;
 SELECT "books".* FROM "books" AS "books" WHERE (("books"."id" = 5));
